9f02699e5add080f1a6503d9732fd7448be5b817
yeabTesfaye <tesfayeyeabsra446@gmail.com>
2024-08-07 10:25:20 +0300

    feat: Implement data shaping for GetEmployee method
    - Added data shaping functionality to the GetEmployee method.
    - Implemented the IDataShaper<T> interface to dynamically select fields.
    - Utilized ExpandoObject to return only the requested fields, reducing payload size.
    - Added support for shaping both single entities and collections of entities.

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404eb187c8da5e9d7e8d3118e6569e892f6d2b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ab184707a71aa500f16e8d1117d296cf234370")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
